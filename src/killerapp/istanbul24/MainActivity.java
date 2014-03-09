5d166c591a5396d5d6ea62133fc287400bb916f8
kinix <kinixtr@gmail.com>
2014-03-09 03:41:49 +0200

    Test commit

--- src/killerapp/istanbul24/MainActivity.java
+++ src/killerapp/istanbul24/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+		// asd
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
@@ -18,7 +19,7 @@ public class MainActivity extends Activity
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return (3==3);
 	}
 
 }
