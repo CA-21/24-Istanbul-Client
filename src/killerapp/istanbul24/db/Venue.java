88564fe2232127b82d85aa8c689a0f54a628857d
xUMR <umurcem20@gmail.com>
2014-04-28 22:44:45 +0300

    ResultActivity working Now Venue implements Parcelable, so that it can be passed through intents.

--- src/killerapp/istanbul24/db/Venue.java
+++ src/killerapp/istanbul24/db/Venue.java
@@ -87,6 +87,19 @@ public final class Venue implements Parcelable
 		this.lastUpdateDate = lastUpdateDate;
 	}
 
+	public Venue(Parcel in)
+	{
+		String[] data = new String[6];
+
+		in.readStringArray(data);
+		this.id = data[0];
+		this.address = data[1];
+		this.name = data[2];
+		this.longitude = Double.parseDouble(data[3]);
+		this.latitude = Double.parseDouble(data[4]);
+		this.lastUpdateDate = data[5];
+	}
+
 	@Override
 	public int describeContents()
 	{
@@ -97,7 +110,21 @@ public final class Venue implements Parcelable
 	@Override
 	public void writeToParcel(Parcel dest, int flags)
 	{
-		// TODO Auto-generated method stub
-		
+		dest.writeStringArray(new String[] { this.id, this.address, this.name,
+				Double.toString(this.longitude),
+				Double.toString(this.latitude), this.lastUpdateDate });
 	}
+
+	public static final Parcelable.Creator CREATOR = new Parcelable.Creator()
+	{
+		public Venue createFromParcel(Parcel in)
+		{
+			return new Venue(in);
+		}
+
+		public Venue[] newArray(int size)
+		{
+			return new Venue[size];
+		}
+	};
 }
